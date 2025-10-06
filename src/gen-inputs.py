2210bbe8c0788cc09c5226b05d362c979a645c01
Elizabeth DuPre <15017191+emdupre@users.noreply.github.com>
2025-10-06 16:41:42 -0400

    Reorganize gen-inputs to improve file handling

--- src/gen-inputs.py
+++ src/gen-inputs.py
@@ -8,131 +8,29 @@ import nibabel as nib
 from nilearn import maskers, masking
 
 
-def _subset_arrays(stim_arr, y_arr, y_labels, x_arr, x_labels, cv_strategy="image"):
+def _load_stim_arrays(data_dir):
     """
     Parameters
     ----------
-    stim_arr : np.arr or list
-        Shape (n_stim, )
-    y_arr : np.arr or list
-        Shape (n_stim, n_features)
-    y_labels : np.arr or list
-        Shape (n_stim,)
-    x_arr : np.arr or list
-        Shape (n_stim, dim_clip_embed)
-    x_labels : np.arr or list
-        Shape (n_stim,)
-    cv_strategy : str
-        Strategy for cross-validation. Must be in ["image", "session", "category"].
-        Note that "session" will divide stimulus repeats s.t. across-session repeats
-        are used for cross-validation, "category" will divide images such that images
-        from the same semantic category are retained within cross-validation folds,
-        and "image" will keep only individual image repetitions within folds,
-        ignoring session effects and category information across folds.
-
-    Returns
-    -------
-    sorted_stim : np.arr
-        Shape (n_unique, )
-    y_matrix : np.arr
-        Shape (3, n_unique, n_features)
-    X_matrix : np.arr
-        Shape (n_unique, dim_clip_embed)
+    data_dir : str
     """
-    cv_strategies = ["image", "session", "category"]
-    if cv_strategy not in cv_strategies:
-        warn_msg = "Unrecognized cross-validation strategy {cv_strategy}"
-        raise UserWarning(warn_msg)
+    clip_feats = np.load(Path(data_dir, "clip-features", "features.npy"))
+    clip_fnames = np.genfromtxt(
+        Path(data_dir, "clip-features", "file_names.txt"), dtype=str
+    )
+    clip_fnames = [Path(f).stem for f in clip_fnames]
 
-    # type coercion
-    stim_arr = np.asarray(stim_arr)
-    y_arr = np.asarray(y_arr)
-    y_labels = np.asarray(y_labels)
-    x_arr = np.asarray(x_arr)
-
-    label, counts = np.unique(stim_arr, return_counts=True)
-    incl_labels = label[counts == 3]
-
-    # filter clip features by incl_labels
-    x_mask = [x_label in incl_labels for x_label in x_labels]
-
-    # consider only stimuli with at least three repetitions
-    y_mask = [s in incl_labels for s in stim_arr]
-    n_unique = np.sum(y_mask)
-
-    # re-sort by stimulus label, after dropping stimuli with < 3 reps
-    sort_idx = np.argsort(stim_arr[y_mask])
-
-    # reshape according to cv_strategy
-    if cv_strategy == "image":
-        rep_y_arr = np.reshape(
-            y_arr[y_mask][sort_idx], (3, n_unique // 3, y_arr.shape[-1]), order="F"
-        )
-        rep_y_labels = np.reshape(
-            y_labels[y_mask][sort_idx], (3, n_unique // 3), order="F"
-        )
-        x_arr = x_arr[x_mask]
-
-    if cv_strategy == "session":
-        ses_y_labels = np.reshape(y_labels[y_mask][sort_idx], (n_unique // 3, 3))
-        ses_sort_idx = np.argsort(ses_y_labels)
-
-        sort_y_labels = np.take_along_axis(ses_y_labels, ses_sort_idx, axis=1)
-        rep_y_labels = np.swapaxes(sort_y_labels, 0, 1)
-
-        ses_y_arr = np.reshape(
-            y_arr[y_mask][sort_idx], (n_unique // 3, 3, y_arr.shape[-1])
-        )
-        sort_y_arr = np.take_along_axis(ses_y_arr, ses_sort_idx[..., None], axis=1)
-        rep_y_arr = np.swapaxes(sort_y_arr, 0, 1)
-        x_arr = x_arr[x_mask]
-
-    if cv_strategy == "category":
-        sorted_stim = stim_arr[y_mask][sort_idx]
-        categ = np.asarray([stim.rsplit("_", 1)[0] for stim in sorted_stim])
-        _, split_idx, counts = np.unique(categ, return_index=True, return_counts=True)
-
-        rep_y_arr = np.split(y_arr[y_mask][sort_idx], split_idx)[
-            1:
-        ]  # dropping first empty split
-        rep_y_labels = np.split(y_labels[y_mask][sort_idx], split_idx)[1:]
-
-        x_unique, x_inv = np.unique(sorted_stim, return_inverse=True)
-        assert np.all(x_unique == incl_labels)  # not best practice but a quick check
-
-        x_arr = np.split(x_arr[x_mask][x_inv], split_idx)[1:]
-        incl_labels = np.split(incl_labels[x_inv], split_idx)[1:]
-
-    return incl_labels, rep_y_arr, rep_y_labels, x_arr
+    return clip_feats, clip_fnames
 
 
-@click.command()
-@click.option("--sub_name", default="sub-01", help="Subject name.")
-@click.option("--roi", default=None, help="Region-of-interest")
-@click.option("--seed", default=0, help="Random seed.")
-@click.option(
-    "--cv_strategy",
-    default=None,
-    help="Strategy for cross-validation. Must be 'image', 'session', or 'category'",
-)
-@click.option(
-    "--data_dir", default="/Users/emdupre/Desktop/things-encode", help="Data directory."
-)
-def main(sub_name, roi, seed, cv_strategy, data_dir):
+def _load_brain_arrays(sub_name, roi, data_dir):
     """
-    Create trialwise inputs for voxelwise encoding models on THINGS data using
-    existing CLIP embeddings (previously generated using thingsvision).
+    Parameters
+    ----------
+    sub_name : str
+    roi : str
+    data_dir : str
     """
-    rois = [None, "EBA", "FFA", "OFA", "pSTS", "MPA", "OPA", "PPA"]
-    if roi not in rois:
-        warn_msg = "Unrecognized ROI {roi}"
-        raise UserWarning(warn_msg)
-
-    sub_names = ["sub-01", "sub-02", "sub-03", "sub-06"]
-    if sub_name not in sub_names:
-        warn_msg = "Unrecognized subject {sub_name}"
-        raise UserWarning(warn_msg)
-
     annot_fname = f"{sub_name}_task-things_desc-perTrial_annotation.tsv"
     beta_fname = f"{sub_name}_task-things_space-T1w_model-fitHrfGLMdenoiseRR_stats-trialBetas_desc-zscore_statseries.h5"
 
@@ -154,12 +52,6 @@ def main(sub_name, roi, seed, cv_strategy, data_dir):
         # (e.g., left FFA is two disconnected pieces of cortex).
         # Worth re-visiting processing steps.
         masker = maskers.NiftiMasker(mask_img=roi_nii).fit()
-
-    clip_feats = np.load(Path(data_dir, "clip-features", "features.npy"))
-    clip_fnames = np.genfromtxt(
-        Path(data_dir, "clip-features", "file_names.txt"), dtype=str
-    )
-    clip_fnames = [Path(f).stem for f in clip_fnames]
 
     annot_df = pd.read_csv(Path(data_dir, "annot", annot_fname), sep="\t")
     annot_df = annot_df.loc[annot_df["exclude_session"] == False]
@@ -197,7 +89,93 @@ def main(sub_name, roi, seed, cv_strategy, data_dir):
         stim_names.append(row["image_name"])
         session_labels.append(row["session"])
 
-    stim_vec, y_matrix, y_sessions, X_matrix = _subset_arrays(
+    return y_vals, stim_names, session_labels
+
+
+def _clean_inputs(stim_arr, y_arr, y_labels, x_arr, x_labels, cv_strategy="image"):
+    """
+    Clean provided brain, feature embedding arrays to
+    (1) drop stimuli with less than three repetitions and
+    (2) sort alphabetically by stimulus label s.t. reptitions
+        are contiguous.
+
+    Parameters
+    ----------
+    stim_arr : np.arr or list
+        Shape (n_stim, )
+    y_arr : np.arr or list
+        Shape (n_stim, n_features)
+    y_labels : np.arr or list
+        Shape (n_stim,)
+    x_arr : np.arr or list
+        Shape (n_stim, dim_clip_embed)
+    x_labels : np.arr or list
+        Shape (n_stim,)
+    cv_strategy : str
+        Strategy for cross-validation. Must be in ["image", "category"].
+        Note that "category" will return `incl_labels` corresponding
+        to image categories (e.g., 'acorn')
+        and "image" will return `incl_labels` corresponding
+        to image identities (e.g., 'acorn_01b').
+
+    Returns
+    -------
+    sorted_stim : np.arr
+        Shape (n_unique, )
+    y_matrix : np.arr
+        Shape (3, n_unique, n_features)
+    X_matrix : np.arr
+        Shape (n_unique, dim_clip_embed)
+    """
+    # type coercion
+    stim_arr = np.asarray(stim_arr)
+    y_arr = np.asarray(y_arr)
+    y_labels = np.asarray(y_labels)
+    x_arr = np.asarray(x_arr)
+
+    label, counts = np.unique(stim_arr, return_counts=True)
+    incl_labels = label[counts == 3]
+
+    # filter clip features by incl_labels
+    x_mask = [x_label in incl_labels for x_label in x_labels]
+
+    # consider only stimuli with at least three repetitions
+    y_mask = [s in incl_labels for s in stim_arr]
+
+    # re-sort by stimulus label, after dropping stimuli with < 3 reps
+    sort_idx = np.argsort(stim_arr[y_mask])
+    sorted_stim = stim_arr[y_mask][sort_idx]
+
+    x_unique, _ = np.unique(sorted_stim, return_inverse=True)
+    assert np.all(x_unique == incl_labels)  # quick sanity check
+
+    incl_y_arr = y_arr[y_mask][sort_idx]
+    incl_y_labels = y_labels[y_mask][sort_idx]
+    incl_x_arr = x_arr[x_mask]
+
+    # reshape according to cv_strategy
+    if cv_strategy == "image":
+        incl_labels = sorted_stim
+
+    if cv_strategy == "category":
+        incl_labels = np.asarray([stim.rsplit("_", 1)[0] for stim in sorted_stim])
+
+    return incl_labels, incl_y_arr, incl_y_labels, incl_x_arr
+
+
+def gen_inputs(sub_name, roi, cv_strategy, data_dir):
+    """
+    Parameters
+    ----------
+    sub_name : str
+    roi : str
+    cv_strategy : str
+    data_dir : str
+    """
+    y_vals, stim_names, session_labels = _load_brain_arrays(sub_name, roi, data_dir)
+    clip_feats, clip_fnames = _load_stim_arrays(data_dir)
+
+    stim_vec, y_matrix, y_sessions, X_matrix = _clean_inputs(
         stim_names,
         y_vals,
         session_labels,
@@ -207,6 +185,74 @@ def main(sub_name, roi, seed, cv_strategy, data_dir):
     )
 
     return stim_vec, y_matrix, y_sessions, X_matrix
+
+
+@click.command()
+@click.option("--sub_name", default="sub-01", help="Subject name.")
+@click.option("--roi", default=None, help="Region-of-interest")
+@click.option(
+    "--cv_strategy",
+    default=None,
+    help="Strategy for cross-validation. Must be 'image' or 'category'",
+)
+@click.option(
+    "--data_dir", default="/Users/emdupre/Desktop/things-encode", help="Data directory."
+)
+def main(sub_name, roi, cv_strategy, data_dir):
+    """
+    Create trialwise inputs for voxelwise encoding models on THINGS data using
+    existing CLIP embeddings (previously generated using thingsvision).
+    """
+    rois = [None, "EBA", "FFA", "OFA", "pSTS", "MPA", "OPA", "PPA"]
+    if roi not in rois:
+        err_msg = "Unrecognized ROI {roi}"
+        raise ValueError(err_msg)
+
+    sub_names = ["sub-01", "sub-02", "sub-03", "sub-06"]
+    if sub_name not in sub_names:
+        err_msg = "Unrecognized subject {sub_name}"
+        raise ValueError(err_msg)
+
+    cv_strategies = ["image", "category"]
+    if cv_strategy not in cv_strategies:
+        err_msg = "Unrecognized cross-validation strategy {cv_strategy}"
+        raise ValueError(err_msg)
+
+    stim_vec, y_matrix, y_sessions, X_matrix = gen_inputs(
+        sub_name, roi, cv_strategy, data_dir
+    )
+
+    out_stim = Path(
+        data_dir, "encoding-inputs", f"{sub_name}_{cv_strategy}_outerCV_groups.txt"
+    )
+    if not out_stim.is_file():
+        out_stim.parent.mkdir(exist_ok=True, parents=True)
+        np.savetxt(out_stim, stim_vec, fmt="%s")
+
+    if roi is not None:
+        out_y_matrix = Path(
+            data_dir, "encoding-inputs", f"{sub_name}_{roi}_brain_responses.npy"
+        )
+        if not out_y_matrix.is_file():
+            out_y_matrix.parent.mkdir(exist_ok=True, parents=True)
+            np.save(out_y_matrix, y_matrix)
+    else:
+        out_y_matrix = Path(
+            data_dir, "encoding-inputs", f"{sub_name}_brain_responses.npy"
+        )
+        if not out_y_matrix.is_file():
+            out_y_matrix.parent.mkdir(exist_ok=True, parents=True)
+            np.save(out_y_matrix, y_matrix)
+
+    out_y_sessions = Path(data_dir, "encoding-inputs", f"{sub_name}_innerCV_groups.txt")
+    if not out_y_sessions.is_file():
+        out_y_sessions.parent.mkdir(exist_ok=True, parents=True)
+        np.savetxt(out_y_sessions, y_sessions, fmt="%s")
+
+    out_X_matrix = Path(data_dir, "encoding-inputs", f"{sub_name}_stim_features.npy")
+    if not out_X_matrix.is_file():
+        out_X_matrix.parent.mkdir(exist_ok=True, parents=True)
+        np.save(out_X_matrix, X_matrix)
 
 
 if __name__ == "__main__":
