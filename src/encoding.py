bd01dfec3af9598aacb612cf83fa0b2b4c22e4e5
Elizabeth DuPre <15017191+emdupre@users.noreply.github.com>
2025-10-08 17:13:53 -0400

    Change pipelining strategy

--- src/encoding.py
+++ src/encoding.py
@@ -1,14 +1,16 @@
 from pathlib import Path
+from collections import defaultdict
 
 import click
 import scipy
+import sklearn
 import numpy as np
 from sklearn import pipeline
 from himalaya.ridge import RidgeCV
 from himalaya.backend import set_backend
-from sklearn.model_selection import GroupKFold
 from sklearn.preprocessing import StandardScaler
 from himalaya.scoring import correlation_score, r2_score
+from sklearn.model_selection import GroupKFold, GridSearchCV, cross_validate
 
 
 backend = set_backend("torch_cuda", on_error="warn")
@@ -47,7 +49,7 @@ def explainable_variance(data, bias_correction=True, do_zscore=True):
 
 
 def ridgeCV_model(
-    X_matrix, y_matrix, groups=None, scoring=r2_score, inner_CV=True, inner_groups=None
+    X_matrix, y_matrix, groups=None, scoring=r2_score, inner_cv=True, inner_groups=None
 ):
     """
     Parameters
@@ -71,17 +73,14 @@ def ridgeCV_model(
         corresponding to scanning session.
         Expected shape  of (n_samples, n_repeats)
     """
-    # Reshape to n_samples, n_features, n_repeats
-    # y = np.moveaxis(y_matrix, [0, 2], [2, 1])
+    # scores = defaultdict(list)
 
-    # Implement standard scaler
     scaler = StandardScaler(with_mean=True, with_std=False)
-    scaler.fit_transform(X_matrix, y_matrix)
+    scaler.fit_transform(X_matrix)
+    scaler.fit_transform(y_matrix)
 
-    alphas = np.logspace(1, 20, 20)
     outer_cv = GroupKFold()
-    inner_cv = GroupKFold()
-
+    alphas = np.logspace(1, 20, 20)
     estimator = RidgeCV(
         alphas=alphas,
         cv=outer_cv,
@@ -90,39 +89,34 @@ def ridgeCV_model(
         ),
     )
 
-    for train, test in outer_cv.split(X_matrix, y_matrix, groups):
-        if inner_cv:
-            if inner_groups is None:
-                raise ValueError("Must provide inner_groups to use inner_cv")
+    if inner_cv:
+        inner_cv = GroupKFold()
+        if inner_groups is None:
+            raise ValueError("Must provide inner_groups to use inner_cv")
 
-            # flatten data to support inner_cv splits
-            n_repeats = y_matrix.shape[-1]
-            X_train = np.repeat(X_matrix[train], n_repeats, axis=0)
-            y_train = np.hstack(y_matrix[train]).T
+        for train, test in outer_cv.split(X_matrix, y_matrix, groups=groups):
+            pass
 
-            # Split inner cross-validation over sessions
-            inner_splits = inner_cv.split(
-                inner_groups[train].ravel(),
-                groups=np.unique(inner_groups[train], return_inverse=True)[-1],
-            )
-
-            # Update estimator with inner cross-validator
-            estimator.set_params(cv=inner_splits)
-
-        else:
-            X_train = X_matrix[train]
-            y_train = y_matrix[train]
-
-        # Fit the regression model on training data
-        estimator.fit(X_train, y_train)
-
-        # Test scores
-        test_prediction = estimator.predict(X_matrix[test])
-        test_score = scoring(y_matrix[test], test_prediction)
-
-        best_alphas = estimator.best_alphas_
-
-    return scores, best_alphas
+    else:
+        sklearn.set_config(enable_metadata_routing=True)
+        scores = cross_validate(
+            estimator,
+            X_matrix,
+            y=y_matrix,
+            cv=outer_cv,
+            # groups=groups,
+            scoring=scoring,
+            error_score="raise",
+            params={"groups": groups},
+        )
+        # for train, test in outer_cv.split(X_matrix, y_matrix, groups=groups):
+        #     # pl.set_params(ridgecv__cv_groups=groups[train])
+        #     pl.fit(X_matrix[train], y_matrix[train])
+        #     fold_score = pl.score(X_matrix[test], y_matrix[test])
+        #     fold_alphas = pl[-1].best_alphas_
+        #     scores["fold_score"].append(fold_score)
+        #     scores["fold_alpha"].append(fold_alphas)
+    return scores
 
 
 @click.command()
@@ -130,39 +124,79 @@ def ridgeCV_model(
 @click.option("--roi", default=None, help="Region-of-interest")
 @click.option("--cv_strategy", default="image", help="Cross-validation strategy")
 @click.option(
-    "--average", is_flag=True, help="Average repeat image presentations across folds."
+    "--average",
+    is_flag=True,
+    help="Average repeat image presentations before encoding. "
+    "Note that this is incompatible with the 'image' cv_strategy",
 )
 @click.option(
     "--data_dir",
     default="/home/emdupre/projects/rrg-pbellec/emdupre/things-encode",
     help="Data directory.",
 )
-def main(sub_name, roi, cv_strategy, average, data_dir):
+@click.option(
+    "--inner_cv",
+    is_flag=True,
+    help="Whether or not to run nested cross-validation, "
+    "cross-validating session number in inner CV.",
+)
+def main(sub_name, roi, cv_strategy, average, data_dir, inner_cv=True):
     """ """
     rois = [None, "EBA", "FFA", "OFA", "pSTS", "MPA", "OPA", "PPA"]
     if roi not in rois:
-        warn_msg = "Unrecognized ROI {roi}"
-        raise UserWarning(warn_msg)
+        err_msg = "Unrecognized ROI {roi}"
+        raise ValueError(err_msg)
 
+    sub_names = ["sub-01", "sub-02", "sub-03", "sub-06"]
+    if sub_name not in sub_names:
+        err_msg = "Unrecognized subject {sub_name}"
+        raise ValueError(err_msg)
+
+    cv_strategies = ["image", "category"]
+    if cv_strategy not in cv_strategies:
+        err_msg = "Unrecognized cross-validation strategy {cv_strategy}"
+        raise ValueError(err_msg)
+
+    if average and (cv_strategy == "image"):
+        err_msg = "Cross-validation strategy 'image' is not compatible with 'average'"
+        raise ValueError(err_msg)
+
+    # TODO: Remove this when tested
     if roi is not None:
         raise NotImplementedError
 
-    sub_names = ["sub-01", "sub-02", "sub-03"]
-    if sub_name not in sub_names:
-        warn_msg = "Unrecognized subject {sub_name}"
-        raise UserWarning(warn_msg)
+    groups = np.loadtxt(
+        Path(data_dir, f"{sub_name}_{cv_strategy}_outerCV_groups.txt"), dtype=np.str_
+    )
+    inner_groups = np.loadtxt(
+        Path(data_dir, f"{sub_name}_innerCV_groups.txt"), dtype=np.str_
+    )
+    X_matrix = np.load(Path(data_dir, f"{sub_name}_stim_features.npy"))
+    if roi is not None:
+        y_matrix = np.load(Path(data_dir, f"{sub_name}_{roi}_brain_responses.npy"))
+    else:
+        y_matrix = np.load(Path(data_dir, f"{sub_name}_brain_responses.npy"))
 
-    mask = Path(
-        data_dir,
-        "masks",
-        f"{sub_name}_task-things_space-T1w_label-brain_desc-unionNonNaN_mask.nii.gz",
+    if average:
+        # cannot use inner CV when averaging
+        inner_cv = False
+        inner_groups = None
+
+        # NOTE: shapes hard-coded for three repetitions, 4174 images, THINGS dataset
+        groups = groups[::3]
+        X_matrix = X_matrix[::3]
+        y_matrix = np.mean(y_matrix.reshape(len(groups), 3, y_matrix.shape[-1]), axis=1)
+
+    scores = ridgeCV_model(
+        X_matrix,
+        y_matrix,
+        groups=groups,
+        scoring=r2_score,
+        inner_cv=inner_cv,
+        inner_groups=inner_groups,
     )
 
-    ev = explainable_variance(y_test)
-
-    np.save(f"{sub_name}_cv-{cv_strategy}_explained_var.npy", ev)
     np.save(f"{sub_name}_cv-{cv_strategy}_r2_scores_clip.npy", scores.cpu())
-    np.save(f"{sub_name}_cv-{cv_strategy}_best_alphas_clip.npy", best_alphas.cpu())
 
 
 if __name__ == "__main__":
